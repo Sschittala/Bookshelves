65290b3c1ab0eef3ece17c21abaeafb4ba44be6c
DimaMdv <skr1ng4m1ng@gmail.com>
2025-11-17 15:35:34 -0600

    Added basic database initialization script

---
+++ backend/app/init_db.py
@@ -0,0 +1,13 @@
+# importing sqlite3
+# This file creates an SQLITE database file named database.db
+
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+
+# To set up the sqlite database
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+connection.commit()
+connection.close()
---
+++ backend/app/schema.sql
@@ -0,0 +1,8 @@
+-- Table definition for storing languages
+CREATE TABLE IF NOT EXISTS Members (
+    id INTEGER PRIMARY KEY,  
+    name TEXT,
+    description TEXT,
+    link TEXT
+);
+
