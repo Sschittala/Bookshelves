1bd5eb9293e57712a9687559fe3beaf8d3a25c41
sai <schit7@uic.edu>
2025-11-21 14:05:57 -0600

    Added a little sample dummy data

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -8,6 +8,10 @@ connection = sqlite3.connect('database.db')
 # To set up the sqlite database
 with open('schema.sql') as f:
     connection.executescript(f.read())
-
+cursor = connection.cursor()
+cursor.execute("""Insert Into members(member_id, name, email)
+               VALUES
+               (190492, 'John Moore', 'jmoore3@gmail.com'),
+               (180333, 'Bob Green', 'bgreen@yahoo.com')""")
 connection.commit()
 connection.close()
